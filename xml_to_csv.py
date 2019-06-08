1ecb48d19ac5a3d40ede22b98d7b32bd2e54f75f
AmadeusW <41280211+Amadeus-Winarto@users.noreply.github.com>
2019-06-08 23:41:47 +0800

    Add files via upload

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -28,6 +28,14 @@ def xml_to_csv(path):
     """
     classes_names = []
     xml_list = []
+	
+	for xml_file in glob.glob(path + "/*.xml"):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall("object"):
+            classes_names.append(member[0].text)
+            
+	dictionary = dict(zip(classes_names, [i for i in range(len(classes_names))]))
     for xml_file in glob.glob(path + "/*.xml"):
         tree = ET.parse(xml_file)
         root = tree.getroot()
@@ -37,7 +45,7 @@ def xml_to_csv(path):
                 root.find("filename").text,
                 int(root.find("size")[0].text),
                 int(root.find("size")[1].text),
-                member[0].text,
+                dictionary[member[0].text],
                 int(member[5][0].text),
                 int(member[5][1].text),
                 int(member[5][2].text),
